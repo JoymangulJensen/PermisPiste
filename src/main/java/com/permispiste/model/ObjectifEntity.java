00fce7232cf567674695b27b6fc583ae81a130ac
matthieuMoisson <matthieu.moisson38@gmail.com>
2017-06-19 11:03:01 +0200

    dao objectif

--- src/main/java/com/permispiste/model/ObjectifEntity.java
+++ src/main/java/com/permispiste/model/ObjectifEntity.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
  */
 @Entity
 @Table(name = "objectif", schema = "permispiste", catalog = "")
-public class ObjectifEntity {
+public class ObjectifEntity implements IEntity {
     private int numobjectif;
     private String libobectif;
 
